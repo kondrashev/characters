2544af2fd19cc1a7e244ba791bc2484c7327dde2
Pavel <pavel@Pavel.local>
2021-11-09 11:46:49 +0200

    start_1

---
+++ src/components/Navigation.js
@@ -0,0 +1,30 @@
+import React, { useContext } from "react";
+import AppBar from "@mui/material/AppBar";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+import { ApplictationContext } from "../index";
+
+export default function Navigation() {
+  const { values, setValues } = useContext(ApplictationContext);
+  const handleChange = (event, newValue) => {
+    setValues({ ...values, tabIndicator: newValue });
+  };
+  return (
+    <Box sx={{ bgcolor: "background.paper", width: "100%" }}>
+      <AppBar position="static">
+        <Tabs
+          value={values.tabIndicator}
+          onChange={handleChange}
+          indicatorColor="secondary"
+          textColor="inherit"
+          variant="fullWidth"
+          aria-label="full width tabs example"
+        >
+          <Tab label="Characters" />
+          <Tab label="Episodes" />
+        </Tabs>
+      </AppBar>
+    </Box>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState, createContext } from "react";
 import ReactDOM from "react-dom";
 import "./App.scss";
+import { context } from "./components/constants/Context";
+import Navigation from "./components/Navigation";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <div className="container_app">
-      <h>Hello word</h>
-    </div>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+export const ApplictationContext = createContext();
+const App = () => {
+  const [values, setValues] = useState(context);
+  return (
+    <ApplictationContext.Provider
+      value={{
+        values: values,
+        setValues: setValues,
+      }}
+    >
+      <div className="container_app">
+        <Navigation />
+      </div>
+    </ApplictationContext.Provider>
+  );
+};
+ReactDOM.render(<App />, document.querySelector("#root"));
